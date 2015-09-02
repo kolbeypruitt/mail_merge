c361c113675df42e319a43aa0d8e33ec9ba2da80
kolbeypruitt <kolbeypruitt@gmail.com>
2015-09-02 16:14:01 -0600

    logic is moving slowly

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,24 @@ var something = require("../lib/something");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Mail Merge' 
+    title: 'Mail Merge'
+   });
+});
+
+router.get('/preview', function(req, res, next) {
+  res.render('index', {
+    title: 'Mail Merge',
+    recipient: "req.body.recipient"
    });
 });
 
 module.exports = router;
+
+
+// router.get('/', function(req, res, next) {
+//   res.render('index', {
+//     title: 'People Table',
+//     tableOutput: data.all });
+// });
+
+// req.body.unHipText

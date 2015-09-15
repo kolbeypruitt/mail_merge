75b72b01e1916abfa506c8f677d67b6d4226644b
kolbeypruitt <kolbeypruitt@gmail.com>
2015-09-15 15:21:30 -0600

    safe place before splitting recipient

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var something = require("../lib/something");
+var backEnd = require("../lib/backEnd");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,30 +10,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/preview', function(req, res, next) {
-  function buildEmailObjects(requestBody) {
- 
-    var arrayRecips = requestBody.recipient.split("\n");
-    var emails = [];
-    for (var i = 0; i < arrayRecips.length; i++) {
-      emails.push({recipient: arrayRecips[i],
-      subject: requestBody.subject,
-      emailBody: requestBody.emailBody})
-    }
-    return emails
-  }
-
-        // {emails:emailObjects = [{
-        //   recipient: req.body.recipient,
-        //   subject: req.body.subject,
-        //   emailBody: req.body.emailBody
-        //  }]}
-
-  var zebras = buildEmailObjects(req.body);
-  console.log(zebras);
-  res.render('index', { emails: zebras });
-  // console.log(req.body.recipient);
-  // console.log(req.body.subject);
-  // console.log(req.body.emailBody);
+  var emailArray = backEnd.buildEmailObjects(req.body);
+  res.render('index', { emails: emailArray });
 });
 
 
